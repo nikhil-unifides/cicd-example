b2a6bb3c17e52b5de55e93c3eb3b7ef216e203b3
nikhil-nilamb <nikhilkmr214@gmail.com>
2021-06-13 22:18:31 +0530

    initial commit

---
+++ src/main/java/com/nikhil/cicd/TestController.java
@@ -0,0 +1,15 @@
+package com.nikhil.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/api")
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/sayhello")
+    public String abc(){
+        return "Hello Nikhil";
+    }
+}
